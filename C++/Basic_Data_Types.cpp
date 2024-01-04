26c53ec1363e25db46d200b07e9e885afeb525c9
meowgalodon <142564761+meowgalodon@users.noreply.github.com>
2024-01-04 10:14:04 +0700

    Add files via upload

---
+++ C++/Basic_Data_Types.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <cstdio>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    // Complete the code.
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin >> a >> b >> c >> d >> e;
+    cout << a << endl;
+    cout << b << endl;
+    cout << c << endl;
+    cout << fixed << setprecision(3) << d << endl;
+    cout << fixed << setprecision(9) << e << endl;
+    return 0;
+}
+
